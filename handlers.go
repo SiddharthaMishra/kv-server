b412c7b8b65897f0a078b7d940122d9c31564bc3
SiddharthaMishra <sidm1999@gmail.com>
2019-04-15 22:53:39 +0530

    added empty handlers

---
+++ handlers.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func getValue(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func putValue(w http.ResponseWriter, r *http.Request) {
+	return
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api", getValue).Methods("GET")
+	r.HandleFunc("/api", putValue).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
